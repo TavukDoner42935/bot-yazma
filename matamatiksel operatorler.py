3cb327a5d09b7f81ec6f75986b4c4e4b72d93d1a
TavukDoner42935 <114801768+TavukDoner42935@users.noreply.github.com>
2022-10-08 11:55:06 +0300

    ETDÖNER

---
+++ matamatiksel operatorler.py
@@ -0,0 +1,30 @@
+"""
+Toplama: +
+Çıkarma: -
+Çarpma: *
+Bölme: /
+Üs: **
+Mod: %
+Tam Bölme: //
+"""
+
+#TOPLAMA
+print(5343+4326)
+
+#ÇIKARMA
+print(5343-4326)
+
+#ÇARPMA
+print(5343*4326)
+
+#BÖLME
+print(5343/4326)
+
+#ÜS
+print(5**6)
+
+#MOD ALMA
+print(40%3)
+
+#TAM BÖLME
+print(40//3)
