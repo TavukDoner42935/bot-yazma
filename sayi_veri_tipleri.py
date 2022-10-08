2b01dadbee60069d9b61676c7ab24980aca94eb3
TavukDoner42935 <114801768+TavukDoner42935@users.noreply.github.com>
2022-10-08 11:28:52 +0300

    tavukdöner

---
+++ sayi_veri_tipleri.py
@@ -0,0 +1,18 @@
+"""
+İki tür veri tipi vardır: integer, float (numbers)
+    int: 1, 75
+    float: 8.5, 12.8
+"""
+
+# TAM SAYILARLA İŞLEM YAPALIM
+
+print(8+7)
+
+# ONDALIK SAYILARLA İŞLEMLER
+
+print(1.9*5.7)
+
+#VERİLERİN TİPİNE BAKALIM type()
+
+print(type(5))
+print(type(5.1))
