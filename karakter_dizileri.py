95ab8ec26b813473e4e6ee2bac59695763b5c4ad
TavukDoner42935 <114801768+TavukDoner42935@users.noreply.github.com>
2022-10-15 11:21:28 +0300

    karakter dizileri

---
+++ karakter_dizileri.py
@@ -0,0 +1,9 @@
+# kullanıcı yaş ve isim değişkenlerini oluşturalım
+
+yas = 14
+ad = "Ahmet"
+
+#kullanıcı bilgileri ekrana yazdıralım
+# -> Adınız: Ahmet , Yaşınız: 14
+
+print("Adınız: "+ ad + " , Yaşınız: " + str(yas))
