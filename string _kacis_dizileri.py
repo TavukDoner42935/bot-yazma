bafc53cbc0707252f45925d303915880b3d961ee
TavukDoner42935 <114801768+TavukDoner42935@users.noreply.github.com>
2022-10-22 11:57:21 +0300

    string_kacis_dizileri

---
+++ string _kacis_dizileri.py
@@ -0,0 +1,19 @@
+"""
+Python’ın gözünde bir başlangıç tırnağı ile bitiş tırnağı arasında yer alan
+her şey bir karakter dizisidir. Ancak bizler metin içerisinde bu tırnakları
+özel bir amaçla kullanmak isteyebiliriz. Bu tür durumlarda kaçış karakteri
+olan ters bölü işaretini (\) kullanarak hatadan kaçarız.
+"""
+
+print("Ahmet, \"Bugün sinemaya gidiyorum.\" dedi.")
+
+print('Yarın adana\'ya gidiyorum')
+
+print("C:\\Users\\Tasarım\\Desktop\\Yeni klasör\\bot-yazma")
+
+# Özel Karakterler
+
+print("Ahmet\nEnes\nOzturk") #alt satır
+
+print("Ahmet\tEnes\tOzturk") #tab
+
