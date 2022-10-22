7303fbe84263c2a1bf34e7415e6d24061d06ad5d
TavukDoner42935 <114801768+TavukDoner42935@users.noreply.github.com>
2022-10-22 10:00:03 +0300

    string metodları

---
+++ string metodları.py
@@ -0,0 +1,35 @@
+okul = "Sancaktepe Teknoloji Anadolu İmamhatip Lisesi"
+
+
+# Tüm karakterleri büyük harf yap: upper()
+
+print (okul.upper())
+
+# Tüm karakterleri küçük harf yap: lower()
+
+print (okul.lower())
+
+# Her kelimenin ilk harfini büyük yap: title()
+
+print (okul.title())
+
+# Karater dizisinin ilk karakterlerini büyük ,diğer karakterlerini küçük harf yapalım: capitalize()
+
+print (okul.capitalize())
+
+# Belirli bir ifade kaç defa yer alır
+
+print (okul.count("a"))
+
+# Büyük harf küçük harf duyarlı
+
+print (okul.lower().count("a"))
+
+# Soldaki ve Sağdaki Boşluk karakterlerini temizleyelim: strip()
+
+ad = input("adınız: ")
+print(ad.strip() + "|")
+
+
+
+
